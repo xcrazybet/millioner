2a469c55357e46e0e8ae03bd952ecb18218c8365
xcrazybet <dilovantalan@gmail.com>
2025-12-24 23:43:53 +0300

    Update firebase-config.js

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,9 +1,10 @@
-// /config/firebase-config.js
+// /public/js/config.js
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyA72Yo_YGqno9PX25p3yQBvyflcaM-NqEM",
+  authDomain: "x-bet-prod-jd.firebaseapp.com",
+  projectId: "x-bet-prod-jd",
+  storageBucket: "x-bet-prod-jd.firebasestorage.app",
+  messagingSenderId: "499334334535",
+  appId: "1:499334334535:web:bebc1bf817e24d9e3c4962",
+  measurementId: "G-PTV4XMYQ6P"
 };
