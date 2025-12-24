9ee2ab0bafa1e8853581d759922530f97b343401
xcrazybet <xcrazybet@users.noreply.github.com>
2025-12-24 21:54:29 +0000

    deploy: 27302f5e20a3526b7bca30fcb2cd32a714eb26d3

---
+++ js/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA72Yo_YGqno9PX25p3yQBvyflcaM-NqEM",
+  authDomain: "x-bet-prod-jd.firebaseapp.com",
+  projectId: "x-bet-prod-jd",
+  storageBucket: "x-bet-prod-jd.firebasestorage.app",
+  messagingSenderId: "499334334535",
+  appId: "1:499334334535:web:bebc1bf817e24d9e3c4962",
+  measurementId: "G-PTV4XMYQ6P"
+};
