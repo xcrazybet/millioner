b2b2953a1ec0b552d260a249549d8afe6b6db8ab
xcrazybet <xcrazybet@users.noreply.github.com>
2025-12-25 22:50:47 +0000

    deploy: 4bb896198a4c12fe3ba7127454225b91e6aee518

---
+++ js/sw.js
@@ -0,0 +1,43 @@
+// Service Worker for offline functionality
+const CACHE_NAME = 'millioner-v1';
+const urlsToCache = [
+  '/',
+  '/dashboard.html',
+  '/login.html',
+  '/register.html',
+  '/admin.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
