1d6b7760d7064d1a43928f9c445fd0c92a91b925
xcrazybet <dilovantalan@gmail.com>
2026-06-09 10:59:13 +0300

    Create config.js

---
+++ admin/js/config.js
@@ -0,0 +1,17 @@
+// ============================================
+// CONFIGURATION
+// ============================================
+
+const SUPABASE_URL = 'https://jnazybaeajyynpyoszmy.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpuYXp5YmFlYWp5eW5weW9zem15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyNzgwMDMsImV4cCI6MjA5Mjg1NDAwM30.P_8V-_s6DQDiYekZX1yaNup48WBRaLhb3ILAkvzTDTY';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA72Yo_YGqno9PX25p3yQBvyflcaM-NqEM",
+    authDomain: "x-bet-prod-jd.firebaseapp.com",
+    projectId: "x-bet-prod-jd",
+    storageBucket: "x-bet-prod-jd.firebasestorage.app",
+    messagingSenderId: "499334334535",
+    appId: "1:499334334535:web:bebc1bf817e24d9e3c4962"
+};
+
+const ALLOWED_ADMINS = ['coca@x.com', 'ooo@x.com'];
